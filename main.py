eab5ecb988b287a6a281ffcae4222ff28a0b3fe6
Dustin Jones <dustinjonesswe@gmail.com>
2025-05-21 11:44:02 -0500

    Added Player functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # Initialize the game
@@ -13,16 +14,41 @@ def main():
     # Initialize the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Initialize groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    # Initialize the clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Create the game loop
     running = True
 
+    # Instantiate the player
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))  # Fill the screen with black
-        pygame.display.flip()
+        # Fill the screen with black
+        screen.fill((0, 0, 0))  
+
+        # Update all game objects
+        updatable.update(dt)
+
+        # Draw all game objects
+        for obj in drawable:
+            obj.draw(screen)
+
+        # Redraw the screen
+        pygame.display.flip()   
+
+        # Limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
